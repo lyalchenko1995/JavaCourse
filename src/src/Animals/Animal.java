16c157b489ae0bb392674ef9f142326238b555fd
Arsenii Lialchenko <arsenii.lialchenko@ukg.com>
2022-10-21 15:52:34 +0300

    Generics&C homework

--- src/src/Animals/Animal.java
+++ src/src/Animals/Animal.java
@@ -1,6 +1,14 @@
 package Animals;
 
 public abstract class Animal implements Actions{
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     String name;
 
     public Animal(String name) {
--- src/src/Animals/Cat.java
+++ src/src/Animals/Cat.java
@@ -1,5 +1,7 @@
 package Animals;
 
+import java.util.Objects;
+
 public class Cat extends Pet {
     String character;
     public Cat(String name, int age, String character) {
@@ -29,5 +31,18 @@ public class Cat extends Pet {
                 ", name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(character, cat.character);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(character);
     }
 }
